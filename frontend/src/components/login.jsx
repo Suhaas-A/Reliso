b1b7f33eecfc390fefbdce50717c8d419da300ab
Suhaas-A <155752982+Suhaas-A@users.noreply.github.com>
2025-03-22 15:26:48 +0530

    Update login.jsx

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -25,22 +25,22 @@ function Login() {
 
     return (
         <>
-            <div style={{position: "absolute", top: '27%', left: '17.5%', width: '32%'}}>
+            <div style={{position: "fixed", top: 200, left: 268}}>
                 <Typography variant="h3" style={{color: 'rgb(0, 100, 0)', fontSize: 58}}><b>Reliso</b></Typography>
                 <Typography variant="h5" style={{fontSize: 27}}>Recycling today for a greener tomorrow.</Typography>
             </div>
             
-            <div class="w3-card-4 w3-white w3-border w3-round-large" style={{'width': '27%', 'height': 350, position: "absolute", top: '18%', left: '54.8%'}}>
-                <TextField style={{width: '92%', margin: 16, marginBottom: 0}} fullWidth class="w3-border w3-round-large" value={username} onChange={function(e) {setUsername(e.target.value)}} placeholder="username"></TextField> <br></br>
-                <TextField style={{width: '92%', margin: 16, marginTop: -10}} fullWidth class="w3-border w3-round-large" value={password} onChange={function(e) {setPassword(e.target.value)}} placeholder="password"></TextField> <br></br>
+            <div class="w3-card-4 w3-white w3-border w3-round-large" style={{'width': 400, 'height': 350, position: "fixed", top: 135, left: 838}}>
+                <TextField style={{width: 368, margin: 16, marginBottom: 0}} fullWidth class="w3-border w3-round-large" value={username} onChange={function(e) {setUsername(e.target.value)}} placeholder="username"></TextField> <br></br>
+                <TextField style={{width: 368, margin: 16, marginTop: -10}} fullWidth class="w3-border w3-round-large" value={password} onChange={function(e) {setPassword(e.target.value)}} placeholder="password"></TextField> <br></br>
                 
-                <Button style={{width: '92%', margin: 16, marginTop: -20, height: '14%'}} variant="contained" fullWidth onClick={function() {submit()}}>submit</Button>
+                <Button style={{width: 368, margin: 16, marginTop: -20, height: 45}} variant="contained" fullWidth onClick={function() {submit()}}>submit</Button>
 
-                <a style={{marginLeft: '30%'}} href="/forget-password">Forgotten Password?</a>
+                <a style={{marginLeft: 130}} href="/forget-password">Forgotten Password?</a>
 
                 <Divider style={{marginTop: 20}}></Divider>
 
-                <Button onClick={function() {window.location.href = '/register'}} style={{width: '59%', margin: '20%', marginBottom: '3.7%', marginTop: '4.1%', height: '14%'}} color="success" variant="contained">Create Account</Button>
+                <Button onClick={function() {window.location.href = 'http://localhost:5173/register'}} style={{width: 200, margin: 100, marginBottom: 16, marginTop: 20, height: 45}} color="success" variant="contained">Create Account</Button>
             </div>
         </>
     )
